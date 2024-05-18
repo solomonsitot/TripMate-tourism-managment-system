ed54ac63a5e26b07dbd296b024f570a77310620d
solomon sitotaw <Solotest922@gmail.com>
2024-05-18 20:09:04 +0300

    adding functionalities

--- frontend/src/pages/Admin/DestinationPost.jsx
+++ frontend/src/pages/Admin/DestinationPost.jsx
@@ -62,7 +62,7 @@ function DestinationPost() {
               <input
                 type="text"
                 id="title"
-                value={title}
+                value={location}
                 onChange={(e) => setLocation(e.target.value)}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 placeholder="Enter blog title"
